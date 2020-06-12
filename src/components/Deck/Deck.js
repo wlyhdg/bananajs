94f81d1f1c4677068ad4e27de51fd16af55c4b9d
Wael Yehdego <waelwc3@gmail.com>
2020-06-11 22:47:07 -0700

    create deckGL sample

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -2,19 +2,20 @@ import React from 'react';
 import DeckGL from '@deck.gl/react';
 import {LineLayer} from '@deck.gl/layers';
 import './Deck.scss';
-import {StaticMap} from 'react-map-gl'
+import {StaticMap} from 'react-map-gl';
+import {MapView, FirstPersonView} from '@deck.gl/core';
 
-const MAPBOX_ACCESS_TOKEN='blah'
+const MAPBOX_ACCESS_TOKEN=process.env.REACT_APP_MAPBOX_TOKEN;
 
 const initialViewState = {
-  longitude: -134,
-  latitude: 34,
+  latitude: 37.3382,
+  longitude: -121.8863,
   zoom:12,
   patch: 0,
   bearing: 0
 };
 
-const data = [{sourcePosition: [-134,34]}, {targetPosition: [-134,34]}];
+const data = [{sourcePosition: [37.3382, -121.8863]}, {targetPosition: [37.3382, -121.8933]}];
 
 export default function Deck () {
   const layers = [
@@ -24,7 +25,10 @@ export default function Deck () {
   return (
     <div className='deck__view'>
       <DeckGL initialViewState={initialViewState} controller={true} layers={layers}>
-        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+        <MapView id="map" width="50%" controller={true}>
+          <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+        </MapView>
+        {/* <FirstPersonView width="50%" x="50%" fovy={50} /> */}
       </DeckGL>
     </div>
   );
