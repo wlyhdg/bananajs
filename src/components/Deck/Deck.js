2368c80f6bae34882a598273f8bc40337d34ffc5
Wael Yehdego <waelwc3@gmail.com>
2020-06-15 22:24:56 -0700

    add deck updates

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
+import {LineLayer, ScatterplotLayer} from '@deck.gl/layers';
 import './Deck.scss';
 import {StaticMap} from 'react-map-gl';
-import {MapView, FirstPersonView} from '@deck.gl/core';
+import {MapView} from '@deck.gl/core';
 
 const MAPBOX_ACCESS_TOKEN=process.env.REACT_APP_MAPBOX_TOKEN;
 
 const initialViewState = {
-  latitude: 37.3382,
-  longitude: -121.8863,
-  zoom:12,
-  patch: 0,
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
   bearing: 0
 };
 
-const data = [{sourcePosition: [37.3382, -121.8863]}, {targetPosition: [37.3382, -121.8933]}];
+const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-121.8863, 37.3382]}];
+
+const layer = new ScatterplotLayer({
+  id: 'bart-stations',
+  data: [
+    {name: 'Colma', passengers: 4214, coordinates: [-122.466233, 37.684638]},
+    {name: 'Civic Center', passengers: 24798, coordinates: [-122.413756,37.779528]}
+  ],
+  stroked: false,
+  filled: true,
+  getPosition: d => d.coordinates,
+  getRadius: d => Math.sqrt(d.passengers),
+  getFillColor: [255, 200, 0]
+});
 
 export default function Deck () {
   const layers = [
-    new LineLayer({id: 'line-layer', data})
+    layer,
+    new LineLayer({id: 'line-layer', data}),
+    // new ScatterplotLayer({data})
   ];
 
   return (
     <div className='deck__view'>
-      <DeckGL initialViewState={initialViewState} controller={true} layers={layers}>
-        <MapView id="map" width="50%" controller={true}>
+      <DeckGL 
+        initialViewState={initialViewState} 
+        controller={true} 
+        layers={layers}
+      >
+        <MapView id="map"  controller={true}>
           <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
         </MapView>
         {/* <FirstPersonView width="50%" x="50%" fovy={50} /> */}
